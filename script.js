33b721ad8e7f692cf5d5ce996383b04fc77e2135
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-08-02 11:08:03 -0600

    211 js built in objects

--- script.js
+++ script.js
@@ -1,52 +1,74 @@
 'use strict';
 
-const Person = function (first, birthYear) {
-  //Instance properties
-  this.firstName = this.firstName;
-  this.birthYear = birthYear;
+// const Person = function (first, birthYear) {
+//   //Instance properties
+//   this.firstName = this.firstName;
+//   this.birthYear = birthYear;
 
-  // Never to this
-  //   this.calcAge = function() {
-  //     console.log(2037 - this.birthYear);
+//   // Never to this
+//   //   this.calcAge = function() {
+//   //     console.log(2037 - this.birthYear);
 
-  //   }
-};
+//   //   }
+// };
 
-new Person('Jonas', 1991);
+// new Person('Jonas', 1991);
 
-// 1. new{} is created
-//2. function is called, this = {}
-//3. {} linked to prototype
-// 4. function automatically return {}
+// // 1. new{} is created
+// //2. function is called, this = {}
+// //3. {} linked to prototype
+// // 4. function automatically return {}
 
-const matilda = new Person('Matilda', 2017);
-const jack = new Person('Jack', 1975);
-console.log(matilda, jack);
+// const matilda = new Person('Matilda', 2017);
+// const jack = new Person('Jack', 1975);
+// console.log(matilda, jack);
 
-console.log(jonas instanceof Person);
+// console.log(jonas instanceof Person);
 
 ///////////////// PROTOTYPES ////////////////
 
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
-};
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
 
-jonas.calcAge();
-matilda.calcAge();
+// jonas.calcAge();
+// matilda.calcAge();
+
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__ === Person.prototype);
+
+// console.log(Person.prototype.isPrototypeOf(jonas));
+// console.log(Person.prototype.isPrototypeOf(matilda));
+// console.log(Person.prototype.isPrototypeOf(Person));
+
+// // .prototype of linked objects
+
+// Person.prototype.species = 'Homo Sapiens';
+// console.log(jonas.species, matilda.species);
+
+// console.log(jonas.hasOwnProperty('firstName'));
+// console.log(jonas.hasOwnProperty('species'));
+
+//////// /////PROTOTYPAL INHERITANCE ON BUILT-IN OBJECTS ///////////////
 
 console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
 
-console.log(Person.prototype.isPrototypeOf(jonas));
-console.log(Person.prototype.isPrototypeOf(matilda));
-console.log(Person.prototype.isPrototypeOf(Person));
+console.dir(Person.prototype.constructor);
 
-// .prototype of linked objects
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
 
-Person.prototype.species = 'Homo Sapiens';
-console.log(jonas.species, matilda.species);
+console.log(arr.__proto__.__proto__);
 
-console.log(jonas.hasOwnProperty('firstName'));
-console.log(jonas.hasOwnProperty('species'));
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
 
-//////// /////PROTOTYPAL INHERITANCE AND THE PROTOTYPE CHAIN ///////////////
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
