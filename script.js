be969770a719502b83e6ab1a7b4aaff3ae97e05c
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-08-02 11:50:57 -0600

    216 js object create

--- script.js
+++ script.js
@@ -121,24 +121,47 @@ class PersonCl {
     this.fullName = fullName;
     this.birthYear = birthYear;
   }
+
   // Instance methods
   // Methods will be added to .prototype property
   calcAge() {
     console.log(2037 - this.birthYear);
   }
+
   greet() {
-    console.log(`Hey' ${this.firstName}`);
+    console.log(`Hey ${this.fullName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  // Static method
+  static hey() {
+    console.log('Hey there 👋');
+    console.log(this);
   }
 }
 
-const jessica = new PersonCl('JessicA', 1996);
+const jessica = new PersonCl('Jessica Davis', 1996);
 console.log(jessica);
 jessica.calcAge();
+console.log(jessica.age);
 
 console.log(jessica.__proto__ === PersonCl.prototype);
 
 // PersonCl.prototype.greet = function () {
-//   console.log(`Hey' ${this.firstName}`);
+//   console.log(`Hey ${this.firstName}`);
 // };
 jessica.greet();
 
@@ -150,7 +173,7 @@ const walter = new PersonCl('Walter White', 1965);
 // PersonCl.hey();
 */
 ////////////// SETTERS AND GETTERS ///////////////
-
+/*
 const account = {
   owner: 'Jonas',
   movements: [200, 530, 120, 300],
@@ -168,5 +191,28 @@ console.log(account.latest);
 
 account.latest = 50;
 console.log(account.movements);
+*/
+////////////// OBJECT CREATE ///////////////
 
-////////////// STATIC METHODS ///////////////
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
