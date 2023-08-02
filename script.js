93d88b540dd3de6bb939d8c79419cc384d2a56be
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-08-02 10:49:44 -0600

    209 prototypes js

--- script.js
+++ script.js
@@ -26,3 +26,27 @@ console.log(matilda, jack);
 console.log(jonas instanceof Person);
 
 ///////////////// PROTOTYPES ////////////////
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototype of linked objects
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+
+//////// /////PROTOTYPAL INHERITANCE AND THE PROTOTYPE CHAIN ///////////////
