6a6c5eac7ab02d6c49e12f01df80dd50441acffa
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-08-02 13:05:14 -0600

    222 js more classes and elements

--- script.js
+++ script.js
@@ -411,7 +411,7 @@ martha.introduce();
 martha.calcAge();
 */
 //////////// INHERITANCE BETWEEN CLASSES: OBJECT.CREATE ///////////////
-
+/*
 const PersonProto = {
   calcAge() {
     console.log(2037 - this.birthYear);
@@ -439,3 +439,48 @@ const jay = Object.create(StudentProto);
 jay.init('Jay', 2010, 'Computer Science');
 jay.introduce();
 jay.calcAge();
+*/
+//////////// ANOTHER CLASS EXAMPLE ///////////////
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner} `);
+  }
+
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+// console.log(acc1);
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+acc1.approveLoan(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
